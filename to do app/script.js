61878d685df31e15f799a6c6ba45ee08e4fa3b7b
Abbas Riaz <abbasriaz100@gmail.com>
2025-09-04 11:39:34 +0500

    learning through project

---
+++ to do app/script.js
@@ -0,0 +1,67 @@
+// const todolist = [];
+
+// function renderTodoList() {
+
+//   let todotask = document.querySelector('.js-todo3').value;
+//   todolist.push(todotask);
+//   console.log(todolist);
+
+//   let todolisthtml = '';
+
+
+// }
+
+
+// function displayTodo() {
+//   let text = document.querySelector('.js-todo1').value;
+//   let testArray = JSON.parse(localStorage.getItem('testArray')) || [];
+//   testArray.push(text);
+//   localStorage.setItem('testArray', JSON.stringify(testArray));
+//   console.log(testArray);
+// }
+
+// function displayTodo1() {
+//   let text = document.querySelector('.js-todo2').value;
+//   let testArray = JSON.parse(localStorage.getItem('testArray')) || [];
+//   testArray.push(text);
+//   localStorage.setItem('testArray', JSON.stringify(testArray));
+//   document.querySelector('.js-todo-tasks').innerHTML = `${testArray.join('<br>')} <br>`;
+
+
+// }
+
+
+
+const todolist = [
+];
+renderTodoList();
+function renderTodoList() {
+  let todolisthtml = '';
+  for (let i = 0; i < todolist.length; i++) {
+    const todo = todolist[i];
+    let name = todo.name;
+    let date = todo.date;
+
+    console.log(name, date);
+    const html = ` <div class="todo-task"> <p class=task>${name} ${date} <\p>  <button class = "delete-button"
+    onclick= "todolist.splice(${i} , 1) ,
+     renderTodoList()"
+    > delete </button>  </div>`;
+    todolisthtml += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML = todolisthtml;
+  console.log(todolisthtml);
+
+}
+
+function addTodo() {
+  let name = document.querySelector('.js-todotask').value
+  let date = document.querySelector('.js-todotask-date').value
+
+
+  todolist.push({
+    name: name,
+    date: date,
+  });
+  renderTodoList();
+}
